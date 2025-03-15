e6c3f7d2b57f8ff0cc36eb3edbdfd100b23c4be6
EliSepulveda <elibetancur18@gmail.com>
2025-03-14 18:41:07 -0700

    pagina

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,84 @@
 import React from "react";
+import NavBar from "./navBar"
+import Jumbotron from "./jumbotron";
+import BootStrapCard from "./card";
+import Footer from "./footer"
+const navBarItems = [
+	(
+		<li className="nav-item" key="1">
+			<a className="nav-link" href="#">Home</a>
+		</li>
+	),
+	(
+		<li className="nav-item" key="2">
+			<a className="nav-link" href="#">About</a>
+		</li>
+	),
+	(
+		<li className="nav-item" key="3">
+			<a className="nav-link" href="#">Services</a>
+		</li>
+	),
+	(
+		<li className="nav-item" key="4">
+			<a className="nav-link" href="#">Contact</a>
+		</li>
+	)
+]
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+const cardsItems = [
+	{
+		imageUrl: "https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-network-placeholder-png-image_3416659.jpg",
+		title: "Card Title 1",
+		description: "Lorem ipsum dolor sit amet, consectetur adinisicing elit Eynlicabo magni sapiente, tempore debitis beatae culpa natus architecto",
+		buttonUrl: "#",
+		buttonLabel: "Find Out More!"
+	},
+	{
+		imageUrl: "https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-network-placeholder-png-image_3416659.jpg",
+		title: "Card Title 2",
+		description: "Lorem ipsum dolor sit amet, consectetur adinisicing elit Eynlicabo magni sapiente, tempore debitis beatae culpa natus architecto",
+		buttonUrl: "#",
+		buttonLabel: "Find Out More!"
+	},
+	{
+		imageUrl: "https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-network-placeholder-png-image_3416659.jpg",
+		title: "Card Title 3",
+		description: "Lorem ipsum dolor sit amet, consectetur adinisicing elit Eynlicabo magni sapiente, tempore debitis beatae culpa natus architecto",
+		buttonUrl: "#",
+		buttonLabel: "Find Out More!"
+	},
+	{
+		imageUrl: "https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-network-placeholder-png-image_3416659.jpg",
+		title: "Card Title 4",
+		description: "Lorem ipsum dolor sit amet, consectetur adinisicing elit Eynlicabo magni sapiente, tempore debitis beatae culpa natus architecto",
+		buttonUrl: "#",
+		buttonLabel: "Find Out More!"
+	}
+]
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<NavBar navBarItems={navBarItems} />
+			<div className="container">
+				<Jumbotron />
+				<div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+					{cardsItems.map((card, index) => (
+						<div className="col" key={index}>
+							<BootStrapCard 
+								imageUrl={card.imageUrl} 
+								title={card.title} 
+								description={card.description} 
+								buttonUrl={card.buttonUrl} 
+								buttonLabel={card.buttonLabel} 
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+			<Footer/>
+		</>
 	);
 };
 
---
+++ src/js/components/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+      <div className="bg-dark d-flex justify-content-center align-items-center mt-5" style={{ height: '50px' }}>
+        <h6 className="text-white m-0">Copyright © Eli Sepulveda</h6>
+      </div>
+    );
+  };
+export default Footer;
